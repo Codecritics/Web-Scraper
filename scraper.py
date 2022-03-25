8ad6fcd7e7fadbc69f7a27d790fe420bc5602896
Tanawa Tsamo Marius <mariustanawa@gmail.com>
2022-03-25 04:02:54 +0100

    Stage 4/5: The Soup is Real

--- scraper.py
+++ scraper.py
@@ -1,14 +1,38 @@
+import string
+
+from bs4 import BeautifulSoup
 from requests import get
 
 if __name__ == '__main__':
-    print("Input the URL:")
-    url = input()
-    r = get(url)
+    URL = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
+    r = get(URL)
+    saved_articles = list()
+    soup = BeautifulSoup(r.content, 'html.parser')
 
-    if str(r.status_code).startswith('3') or str(r.status_code).startswith('4') or str(r.status_code).startswith('5'):
-        print(f"The URL returned {r.status_code}!")
-    else:
-        page_content = r.content
-        with open('source.html', 'wb') as file:
-            file.write(page_content)
-        print("Content saved.")
+    article_list = soup.find("ul", class_="app-article-list-row")
+    articles_types = [type_.text.strip('\n') for type_ in
+                      article_list.find_all_next("span", {"data-test": "article.type"})]
+
+    news_indexes = [i for i in range(len(articles_types)) if articles_types[i] == "News"]
+
+    punctuation_trans = str.maketrans('', '', string.punctuation.replace('_', ''))
+    articles_titles = article_list.find_all_next("a", class_="c-card__link u-link-inherit")
+
+    articles_links = article_list.find_all_next('a', {"data-track-action": "view article"})
+    for news_index in news_indexes:
+        article_link = "https://www.nature.com" + articles_links[news_index].get('href')
+        article_content = get(article_link).content
+        soup_article = BeautifulSoup(article_content, 'html.parser')
+
+        content_to_write = soup_article.find("div", class_="c-article-body u-clearfix").text.encode("UTF-8")
+
+        article_filename = articles_titles[news_index].text.rstrip().lstrip().replace(' ', '_').translate(
+            punctuation_trans) + ".txt"
+
+        with open(article_filename,
+                  'wb') as file:
+            file.write(content_to_write)
+        saved_articles.append(article_filename)
+        
+    print("Saved articles:  ")
+    print(saved_articles)
